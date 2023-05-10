f41a39ca47ffcf6e7e5f7da2a031e63faa036735
Pawel Godula <bolomaster7@gmail.com>
2023-05-10 10:39:45 +0200

    add authModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,12 +10,14 @@ import * as cors from 'cors';
 import { AuthorsModule } from './authors/authors.module';
 import { BooksModule } from './books/books.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     AuthorsModule, 
     BooksModule, 
-    UsersModule],
+    UsersModule,
+    AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
