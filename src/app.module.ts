c10908d61a410a7667833b83dd2b9e3f7a0d1c63
Pawel Godula <bolomaster7@gmail.com>
2023-05-10 11:00:28 +0200

    prismaModule changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,13 +11,15 @@ import { AuthorsModule } from './authors/authors.module';
 import { BooksModule } from './books/books.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     AuthorsModule, 
     BooksModule, 
     UsersModule,
-    AuthModule],
+    AuthModule,
+   PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
