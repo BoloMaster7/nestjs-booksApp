b11ed290784020768930b4df3a2b8036aa715621
Pawel Godula <bolomaster7@gmail.com>
2023-05-10 11:34:01 +0200

    register in auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { 
+  Controller,
+  Post,
+  Body
+ } from '@nestjs/common';
+ import { RegisterDTO } from './dtos/register.dto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Post('/register')
+  public register(@Body() userData: RegisterDTO) {
+    return this.authService.register(userData);
+  }
 }
